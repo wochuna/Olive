98a66580474fba918afdcf93eee71032308d8cac
wochuna <wochunayvonne@gmail.com>
2025-01-30 13:37:09 +0300

    ignore

--- qustionnaire/morning/models.py
+++ qustionnaire/morning/models.py
@@ -1,3 +1,19 @@
+"""
+This models describe the table that stores feedback including gender,months visited and experience
+"""
+
 from django.db import models
 
 # Create your models here.
+
+class Client(models.Model):
+    id = models.AutoField(primary_key=True)
+    gender = models.CharField(max_length=11)
+    month = models.CharField(max_length=11)
+    experience = models.CharField(max_length=11)
+
+class Meta:
+    db_table = 'client'
+
+def __str__(self):
+    return f'{self.gender} - {self.month} - {self.experience}'
